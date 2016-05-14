616468f2b657a26732784e4de46dcface1a433f3
jamierose <jrose111284@gmail.com>
2016-05-14 11:41:44 +0100

    footer added

--- index.php
+++ index.php
@@ -89,28 +89,28 @@
 
   <form>
     <h1>Contact Us</h1>
-  <div class="form-group row">
-    <label for="inputEmail3" class="col-sm-2 form-control-label">First Name *</label>
-    <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail3" placeholder="First Name">
+    <div class="form-group row">
+        <label for="inputEmail3" class="col-sm-2 form-control-label">First Name *</label>
+      <div class="col-sm-10">
+        <input type="name" class="form-control" id="inputEmail3" placeholder="First Name">
+      </div>
     </div>
-  </div>
-  <div class="form-group row">
-    <label for="inputPassword3" class="col-sm-2 form-control-label">Last Name *</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword3" placeholder="Last Name">
+    <div class="form-group row">
+      <label for="inputPassword3" class="col-sm-2 form-control-label">Last Name *</label>
+      <div class="col-sm-10">
+        <input type="last-name" class="form-control" id="inputPassword3" placeholder="Last Name">
     </div>
-  </div>
+    </div>
   <div class="form-group row">
     <label for="inputPassword3" class="col-sm-2 form-control-label">Email *</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword3" placeholder="Email">
+      <input type="email" class="form-control" id="inputPassword3" placeholder="Email">
     </div>
   </div>
   <div class="form-group row">
     <label for="inputPassword3" class="col-sm-2 form-control-label">Contact Number</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword3" placeholder="Contact Number">
+      <input type="number" class="form-control" id="inputPassword3" placeholder="Contact Number">
     </div>
   </div>
   <div class="form-group row">
@@ -120,7 +120,6 @@
     </div>
   </div>
   <div class="form-group row">
-    <!-- <label class="col-sm-2">Checkbox</label> -->
     <div class="col-sm-10">
       <div class="checkbox">
         <label>
@@ -147,5 +146,13 @@
 
 
 </body>
+<footer>
+  <div class="container foot">
+    <p>
+      Copyrights &copy; 2016, All rights reserved
+    </p>
+
+  </div>
+</footer>
 
 </html>
