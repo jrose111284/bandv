c3f58465f99531e4fdc51f29de0364995da13fb0
jamierose <jrose111284@gmail.com>
2016-05-20 13:27:57 +0100

    removed background image on contact us page

--- contact_us.php
+++ contact_us.php
@@ -39,7 +39,7 @@
     <div class="container">
       <div class="row">
         <div class="col-md-12">
-          <div class="page"><h1>Welcome To Our Contact Page</h1></div>
+          <h1>Welcome To Our Contact Page</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         </div>
       </div>
