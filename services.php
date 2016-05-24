e90dbc07a4a431aea567e3046b08b6cdcdf20050
jamierose <jrose111284@gmail.com>
2016-05-24 16:51:00 +0100

    added more to text middle section on index

--- services.php
+++ services.php
@@ -41,9 +41,11 @@
     <div class="container">
       <div class="row">
         <div class="col-md-12">
-          <h1>Welcome to Our Services Page</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-          <a href="index.php" class="btn btn-primary">Back</a>
+          <div class="text">
+            <h1>lorem ipsum dolor sit amet</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <a href="blog.php" class="btn btn-primary">Read More</a>
+          </div>
         </div>
       </div>
     </div>
